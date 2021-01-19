bb84f91a7aaf60faf79c91533aa9f9e0c59e9d2e
James Chege <jymeschege@gmail.com>
2021-01-19 19:50:21 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
+import cors from 'cors';
+
+import HttpError from './utils/http-error';
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+if (process.env.NODE_ENV != 'test') {
+    app.listen(PORT, () => console.log(`Running on localhost:${PORT}`));
+}
+
+module.exports = app;
