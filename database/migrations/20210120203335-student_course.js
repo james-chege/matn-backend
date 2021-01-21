c00d7ed3b65891e8afb8a56f5a78fff6f05e4073
James Chege <jymeschege@gmail.com>
2021-01-21 19:38:26 +0300

    add student course migrations

---
+++ database/migrations/20210120203335-student_course.js
@@ -0,0 +1,34 @@
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('StudentCourses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      studentId: {
+        type: Sequelize.INTEGER
+      },
+      courseId: {
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('StudentCourses');
+  }
+};
